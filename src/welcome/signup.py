82948cf58b31d9d0a544b4d5f16f94f1bce26e01
sivapy <imsiva89@gmail.com>
2018-03-05 10:03:38 +0530

    signup

---
+++ src/welcome/signup.py
@@ -0,0 +1,11 @@
+from pyjamas.ui.RootPanel import RootPanel
+from pyjamas.ui.Label import Label
+from pyjamas.ui.Button import Button
+from pyjamas.ui.HTML import HTML
+from pyjamas.ui.VerticalPanel import VerticalPanel
+from pyjamas.ui.FormPanel import FormPanel
+from pyjamas.ui.TextBox import TextBox
+from pyjamas.ui.PasswordTextBox import PasswordTextBox
+from pyjamas import Window
+from pyjamas.ui.Anchor import Anchor
+
